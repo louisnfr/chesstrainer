491e79d6810e620fdfc703e79dd3d80919ead89e
louisnfr <louis.raffin@gmail.com>
2025-07-15 15:51:06 +0200

    project setup

---
+++ lib/playground.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Playground extends StatelessWidget {
+  const Playground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Playground')),
+      body: const SafeArea(child: Column(children: [Text('oe')])),
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData primaryTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromRGBO(254, 12, 15, 1),
+  ),
+);
